875896729f220e6f5efadda7fd878b7c36189406
filipw <wojcieszyn@gmail.com>
2015-09-15 14:56:37 +0200

    first commit

---
+++ src/ScriptCsRepl/AspNet5AssemblyResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Dnx.Runtime;
+using ScriptCs.Contracts;
+
+namespace ScriptCsRepl
+{
+    public class AspNet5AssemblyResolver : IAssemblyResolver
+    {
+        private readonly ILibraryManager _libraryManager;
+
+        public AspNet5AssemblyResolver(ILibraryManager libraryManager)
+        {
+            _libraryManager = libraryManager;
+        }
+
+        public IEnumerable<string> GetAssemblyPaths(string path, bool binariesOnly = false)
+        {
+            var assemblies = _libraryManager.GetLibraries().SelectMany(x => x.Assemblies).Select(x =>
+            {
+                try
+                {
+                    return Assembly.Load(x);
+                }
+                catch (Exception)
+                {
+                }
+
+                return null;
+            });
+
+            return assemblies.Where(x => !string.IsNullOrEmpty(x?.Location)).Select(x => x.Location);
+        }
+    }
+}
---
+++ src/ScriptCsRepl/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ScriptCs;
+using ScriptCs.Hosting;
+using ScriptCs.Contracts;
+using ScriptCs.Engine.Roslyn;
+using Microsoft.Dnx.Runtime;
+using ScriptCs.Engine.Mono;
+
+namespace ScriptCsRepl
+{
+    public class Program
+    {
+        private static readonly bool IsMono = Type.GetType("Mono.Runtime") != null;
+        private readonly ILibraryManager _libraryManager;
+
+        public Program(ILibraryManager libraryManager)
+        {
+            _libraryManager = libraryManager;
+        }
+
+        private static readonly IConsole Console = new ScriptConsole();
+
+        public void Main(string[] args)
+        {
+            var assemblyReslover = new AspNet5AssemblyResolver(_libraryManager);
+            var scriptServicesBuilder =
+                new ScriptServicesBuilder(Console, new DefaultLogProvider()).Cache(false).Repl(true);
+
+            scriptServicesBuilder = IsMono ? scriptServicesBuilder.ScriptEngine<MonoScriptEngine>() : scriptServicesBuilder.ScriptEngine<RoslynScriptEngine>();
+
+            ((ScriptServicesBuilder)scriptServicesBuilder).Overrides[typeof (IAssemblyResolver)] = assemblyReslover;
+
+            var scriptcs = scriptServicesBuilder.Build();
+
+            scriptcs.Repl.Initialize(Enumerable.Empty<string>(), Enumerable.Empty<IScriptPack>());
+            scriptcs.Repl.AddReferences(assemblyReslover.GetAssemblyPaths(string.Empty).ToArray());
+
+            try
+            {
+                while (ExecuteLine(scriptcs.Repl))
+                {
+                }
+
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = oldColor;
+            }
+
+        }
+
+        private static bool ExecuteLine(IRepl repl)
+        {
+            Console.Write(string.IsNullOrWhiteSpace(repl.Buffer) ? "> " : "* ");
+
+            try
+            {
+                var line = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    repl.Execute(line);
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
